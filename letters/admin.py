7dd9139f6868c3dd51a42075aa815bd34eebb1c0
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-03-06 17:57:51 -0500

    Added Article model. Not linked to letter currently.

--- letters/admin.py
+++ letters/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Letter
+from .models import Letter, Article
 
 # Register your models here.
 
 
 admin.site.register(Letter)
+admin.site.register(Article)
--- letters/models.py
+++ letters/models.py
@@ -4,11 +4,21 @@ from django.db import models
 
 # Create your models here.
 
+class Article(models.Model):
+    publication_date = models.DateField()
+    lead_paragraph = models.TextField()
+
+    def __str__(self):
+        return self.lead_paragraph
+
 
 class Letter(models.Model):
     text = models.TextField()
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    published = models.BooleanField(default=False)
+    # about_article = models.ForeignKey(
+    #    Article, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.text
