a7f82cc065f329c0b437cd5a6c0024b88587bbf1
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-03-16 11:04:28 -0400

    Added additional identifiers to letter in LetterSerializer

--- letters/serializers.py
+++ letters/serializers.py
@@ -6,8 +6,10 @@ from .models import Letter
 class LetterSerializer(serializers.ModelSerializer):
     # article = ArticleSerializer(read_only=True)
     author_name = serializers.ReadOnlyField(source='author.username')
+    search_term = serializers.ReadOnlyField(source='about_article.search_term')
 
     class Meta:
         model = Letter
-        fields = ('id', 'text', 'author', 'author_name',)
+        fields = ('id', 'text', 'author', 'author_name',
+                  'about_article', 'published', 'votes', 'search_term')
         read_only_fields = ('author',)
